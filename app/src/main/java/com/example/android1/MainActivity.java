c783d8269c0b1e9cbbee02423dcfd294454a34a9
SeniorARA <denkarp123@mail.ru>
2021-06-22 12:30:30 +0600

    3 seconds colors constraint layouts,and new Thread

--- app/src/main/java/com/example/android1/MainActivity.java
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout b_1,b_2,b_3;
     private Button btn;
     public String start = "Start";
+    private boolean start_stop=false;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,55 @@ public class MainActivity extends AppCompatActivity {
         b_1=findViewById(R.id.dumb_1);
         b_2=findViewById(R.id.dumb_2);
         b_3=findViewById(R.id.dumb_3);
-        btn=findViewById(R.id.Button);
+        btn=findViewById(R.id.btn_1);
     }
 
     public void OnClickStart(View view){
-        int lol;
+    if (!start_stop){
+        start_stop=true;
+        btn.setText("Stop");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (start_stop){
+                    counter++;
+                    switch (counter){
+                        case 1:
+                            b_1.setBackgroundColor(getResources().getColor(R.color.red));
+                            b_2.setBackgroundColor(getResources().getColor(R.color.grey));
+                            b_3.setBackgroundColor(getResources().getColor(R.color.grey));
+                            break;
+                        case 2:
+                            b_1.setBackgroundColor(getResources().getColor(R.color.grey));
+                            b_2.setBackgroundColor(getResources().getColor(R.color.yellow));
+                            b_3.setBackgroundColor(getResources().getColor(R.color.grey));
+                            break;
+                        case 3:
+                            b_1.setBackgroundColor(getResources().getColor(R.color.grey));
+                            b_2.setBackgroundColor(getResources().getColor(R.color.grey));
+                            b_3.setBackgroundColor(getResources().getColor(R.color.green));
+                            counter=0;
+                            break;
+                    }
+                    try {
+                        Thread.sleep(3000);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+
+
+
+
+
+        }else {
+        start_stop=false;
+        btn.setText("Start");
+    }
+    }
     }
 
-}
